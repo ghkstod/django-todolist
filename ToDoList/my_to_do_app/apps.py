0846ae7c9fcda79ce90fb6c1bd45bc0066fbe29e
hanwool <na990312@naver.com>
2024-01-26 14:04:29 +0900

    2nd commit: list up down func

--- ToDoList/my_to_do_app/apps.py
+++ ToDoList/my_to_do_app/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class MyToDoAppConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "my_to_do_app"
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'my_to_do_app'
